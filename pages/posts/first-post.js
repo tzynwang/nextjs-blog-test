13485bd48e29b484b20e6a1b935e354315842597
Tzu Yin <tzyn.wang@gmail.com>
2022-06-10 20:54:45 +0800

    [feat] add third-party JS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,22 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function FirstPost() {
+  const handleOnLoad = () => {
+    console.log('script loaded correctly, window.FB has been populated');
+  };
+
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={handleOnLoad}
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
