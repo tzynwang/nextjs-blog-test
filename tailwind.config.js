0f9b88c986ab265cd7e69b0c69fd3d4d4b5b5d06
Tzu Yin <tzyn.wang@gmail.com>
2022-06-10 21:31:04 +0800

    [chore] add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
     // For the best performance and to avoid false positives,
     // be as specific as possible with your content configuration.
-  ]
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['forest']
+  }
 };
