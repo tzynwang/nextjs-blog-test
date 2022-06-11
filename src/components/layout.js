e546b1386f7050e9e2be77b4ef9bf471707419c5
Tzu Yin <tzyn.wang@gmail.com>
2022-06-11 21:33:32 +0800

    [feat] apply alias in src files

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,8 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import utilStyles from '@Styles/utils.module.css';
 import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 
 const name = 'Charlie';
 export const siteTitle = 'Next.js Blog Tutorial';
