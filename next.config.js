3a10c5ff84cd8a02df611ee2069255c8fa6c7701
Tzu Yin <tzyn.wang@gmail.com>
2022-06-12 11:43:42 +0800

    [chore] add mdx support

--- next.config.js
+++ next.config.js
@@ -2,13 +2,25 @@ const path = require('path');
 const alias = require('./config/alias');
 
 module.exports = {
-  webpack: (config) => {
-    // Important: return the modified config
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader'
+        }
+      ]
+    });
+
     config.resolve.alias = {
       ...config.resolve.alias,
       // your aliases
       ...alias
     };
+
+    // Important: return the modified config
     return config;
-  }
+  },
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts']
 };
